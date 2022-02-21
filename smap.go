05c42e7bf4f8d8631497e526d031949def5ca14c
Roman Matskus <rmatskus@detmir.ru>
2022-02-21 17:52:00 +0300

    add lib

---
+++ smap.go
@@ -0,0 +1,70 @@
+package smap
+
+import (
+	"sync"
+
+	log "github.com/sirupsen/logrus"
+	"go.k6.io/k6/js/modules"
+)
+
+func init() {
+	modules.Register("k6/x/smap", New())
+}
+
+type SMap struct {
+	ch chan interface{}
+	mp *sync.Map
+}
+
+func New() *SMap {
+	res := &SMap{
+		ch: make(chan interface{}, 100),
+		mp: new(sync.Map),
+	}
+	go worker(res)
+	return res
+}
+
+func (s *SMap) Store(k interface{}, v interface{}) {
+	s.mp.Store(k, v)
+}
+
+func (s *SMap) Load(k interface{}) (interface{}, bool) {
+	return s.mp.Load(k)
+}
+
+func (s *SMap) Delete(k interface{}) {
+	s.mp.Delete(k)
+}
+
+func (s *SMap) LoadAndDelete(k interface{}) (interface{}, bool) {
+	return s.mp.LoadAndDelete(k)
+}
+
+func (s *SMap) LoadOrStore(k interface{}, v interface{}) (interface{}, bool) {
+	return s.mp.LoadOrStore(k, v)
+}
+
+func (s *SMap) Sequential() interface{} {
+	return <-s.ch
+}
+
+func worker(s *SMap) {
+	var l = 0
+	defer func() {
+		if err := recover(); err != nil {
+			log.Error("Worker recover panic ", err)
+		}
+		go worker(s)
+	}()
+	for {
+		s.mp.Range(func(k, v interface{}) bool {
+			if len(s.ch) <= l {
+				s.ch <- v
+			}
+			l++
+			return true
+		})
+		l = 0
+	}
+}
